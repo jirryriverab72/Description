d6b01d1cd65b93258b2be92fe0c401ec3296a485
Jirryr Iverab72 <131536815+jirryriverab72@users.noreply.github.com>
2023-05-14 12:22:11 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+a = 10
+b = 3
+
+sum = a + b
+difference = a - b
+product = a * b
+quotient = a / b
+remainder = a % b
+
+print(sum)          # 13
+print(difference)   # 7
+print(product)      # 30
+print(quotient)     # 3.3333333333333335
+print(remainder)    # 1
